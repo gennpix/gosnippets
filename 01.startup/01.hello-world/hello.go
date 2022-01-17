d0c6bfbf6fd9d2ce7fa6b4f017bc9ce51b8a3c55
xin <gennpix@163.com>
2022-01-17 17:20:14 +0800

    add webserver and build executable object
    Signed-off-by: xin <gennpix@163.com>

--- 01.startup/01.hello-world/hello.go
+++ 01.startup/01.hello-world/hello.go
@@ -1,3 +1,28 @@
+// 1. 运行: go run hello.go
+// 2. 不同环境下的编译：
+//    - Mac环境:
+//        - Mac上运行: go build hello.go
+//        - Linux上运行: CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build hello.go
+//        - Windows上运行: CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build hello.go
+//    - Linux环境:
+//        - Mac上运行: CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build hello.go
+//        - Linux上运行: go build hello.go
+//        - Windows上运行: CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build hello.go
+//    - Windows环境:
+//       - Mac上运行:
+//		  SET CGO_ENABLED=0
+//		  SET GOOS=darwin3
+//		  SET GOARCH=amd64
+//		  go build hello.go
+//      - Linux上运行:
+//        SET CGO_ENABLED=0
+//        SET GOOS=linux
+//        SET GOARCH=amd64
+//        go build hello.go
+//      - Windows上运行: go build hello.go
+//
+// 3. 指定可执行文件路径/名称： go build -o hello_world hello.go
+
 package main
 
 import "fmt"
